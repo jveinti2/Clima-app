c006e885130726615ebbcef8b55d1830db31e45d
Jhonatan Becerra <jsbpdeveloper@gmail.com>
2021-11-23 21:45:06 -0500

    corrigiendo obtención de api y creando estructura html

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ window.addEventListener('load', ()=>{
     
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(poss =>{
-            lon = poss.coords.longitude
-            lat = poss.coords.latitude
+            // lon = poss.coords.longitude
+            // lat = poss.coords.latitude
 
-            const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=cd29f37f99cae462d478a2535677c0d5
+            //Referencia elementos del Dom
+            let temperaturaValor = document.getElementById('temperatura-valor')
+            let temperaturaDescripcion = document.getElementById('temperatura-descripcion')
+            let ubicacion = document.getElementById('ubicacion')
+            let iconoAnimado = document.getElementById('icono-animado')
+            let vientoVelocidad = document.getElementById('viento-velocidad')
 
-            console.log(url)
+            //Captura de API
+            const url =  `https://api.openweathermap.org/data/2.5/weather?q=Barrancabermeja&appid=cd29f37f99cae462d478a2535677c0d5`
+            
+            
+            fetch(url)
+                .then(response => {return response.json() })
+                .then(data => {
+                    console.log(data)
+                    let temp = Math.round(data.main.temp)
+                    temperaturaValor.textContent = `${temp} °C`
+                })
+                .catch(error => {
+                    console.log(error)
+                })
         })
     }
 })
